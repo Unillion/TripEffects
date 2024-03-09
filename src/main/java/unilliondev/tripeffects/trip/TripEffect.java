dbcee4185d625ac432dbc647d536d9710d471acb
Unillion <114824496+Unillion@users.noreply.github.com>
2024-03-10 02:15:48 +0500

    Update TripEffect.java

--- src/main/java/unilliondev/tripeffects/trip/TripEffect.java
+++ src/main/java/unilliondev/tripeffects/trip/TripEffect.java
@@ -4,6 +4,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public interface TripEffect {
-    void sendTrip(Player player) throws ReflectiveOperationException;
+    void sendTrip(Player player);
     void init(JavaPlugin main);
 }
